1e615ba1517ac6061ad522175590255676b698b0
xsd <saleksey67@gmail.com>
2017-05-09 13:26:27 +0300

    Added the concept of time. Now the application displays the start time of the test, the end time of the test, and the duration of the test. Added test ready for test. Enabled Serialization.

--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
 
 /**
  * Created by xsd on 27.04.2017 with love.
@@ -11,7 +15,7 @@ import java.util.ArrayList;
  * @version 0.1
  */
 
-public class main {
+public class main implements Serializable {
     //Список заданий Map с ключом и строкой
     //Вывод на экран задания
     //Проверка решения
@@ -32,6 +36,13 @@ public class main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        //Timer
+        Date sdate;  //start timer val
+        long edate;  //end timer val
+        long ldate;  //end and start diff
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        SimpleDateFormat ldateFormat = new SimpleDateFormat("mm:ss.SSS");
+
         //Выводим список вопросы по очереди
         ArrayList<String> list = setList();
 
@@ -39,41 +50,65 @@ public class main {
         char[] vals;
         char o;
 
-        //для каждого значения в списке выполнить метода anw
-        for (int i = 0; i < list.size(); i++) {
+        //Are you ready?
+        System.out.println("Are you ready? (y/n)");
+        String waiter = reader.readLine();
 
-            vals = (list.get(i).toCharArray());
-            x = Character.getNumericValue(vals[0]);
-            y = Character.getNumericValue(vals[2]);
+        if (waiter.equals("y") || waiter.equals("yes")) {
 
-            o = vals[1];
-            z = x * y - x - y;
+            System.out.println("Time Start!");
 
-            if (o == '+') {
-                z = x + y;
+            sdate = new Date();
+            //для каждого значения в списке выполнить метода anw
+            for (int i = 0; i < list.size(); i++) {
+
+                vals = (list.get(i).toCharArray());
+                x = Character.getNumericValue(vals[0]);
+                y = Character.getNumericValue(vals[2]);
+
+                o = vals[1];
+                z = x * y - x - y;
+
+                if (o == '+') {
+                    z = x + y;
+                }
+                if (o == '*') {
+                    z = x * y;
+                }
+                if (o == '/') {
+                    z = x / y;
+                }
+                if (o == '-') {
+                    z = x - y;
+                }
+                System.out.println(list.get(i));
+                answ = Integer.parseInt(reader.readLine());
+                if (answ == z) {
+                    System.out.println("ok, answer is: " + z + " next...");
+                }
+                if (answ != z) {
+                    System.out.println("bad... one more time..");
+                    i = i - 1;
+                }
             }
-            if (o == '*') {
-                z = x * y;
+
+            if (answ == -1) {
+                System.out.println("e");
             }
-            if (o == '/') {
-                z = x / y;
-            }
-            if (o == '-') {
-                z = x - y;
-            }
-            System.out.println(list.get(i));
-            answ = Integer.parseInt(reader.readLine());
-            if (answ == z) {
-                System.out.println("ok, answer is: " + z + " next...");
-            }
-            if (answ != z) {
-                System.out.println("bad... one more time..");
-                i = i - 1;
-            }
+            edate = new Date().getTime();
+            ldate = edate - sdate.getTime();
+
+            System.out.println("=====-----FINISH-----=====");
+            System.out.println("Start at: " + dateFormat.format(sdate.getTime()));
+            System.out.println("End at: " + dateFormat.format(edate));
+            System.out.println("Test time: " + ldateFormat.format(ldate));
+            System.out.println("=====----------------=====");
         }
-        if (answ == -1) {
-            System.out.println("e");
+
+        if (waiter.equals("n") || waiter.equals("no")) {
+            System.out.println("The end");
         }
+
     }
 }
 
