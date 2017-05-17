99051c3d790dd1823788859149eceed792f12138
AlbertoMalih <gu.oleg2015@yandex.ru>
2017-05-17 16:31:32 +0300

    Update TestO1.java

--- src/TestO1.java
+++ src/TestO1.java
@@ -7,7 +7,7 @@ public class TestO1 {
     public static void main(String[] args) throws Exception {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 //        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-        Connection connection = DriverManager.getConnection("jdbc:sqlserver://mysql.alex-savin.myjino.ru:3306","046470945_RutaK", "2017!RutaK");
+        Connection connection = DriverManager.getConnection("jdbc:sqlserver://mysql.alex-savin.myjino.ru:3306","риои", "июри");
     }//81.177.140.251
 }//83.220.237.129//
 /*
