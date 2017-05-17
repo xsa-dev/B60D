bcc2414a6cd49903182995890313468a5ccfa8e5
AlbertoMalih <gu.oleg2015@yandex.ru>
2017-05-17 16:16:00 +0300

    this code for test mysql( when show not exception togda very good and tell me about it

---
+++ src/TestO1.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+/**
+ * Created by Administrator1 on 17.05.2017.
+ */
+public class TestO1 {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        Connection connection = DriverManager.getConnection("jdbc:sqlserver://mysql.alex-savin.myjino.ru:3306;DatabaseName=alex-savin_trutak","046470945_RutaK", "2017!RutaK");
+    }//81.177.140.251
+}//83.220.237.129
+/*
+Provider / driver: mysql
+Host:
+mysql.alex-savin.myjino.ru
+Db: alex-savin_trutak
+Login: 046470945_RutaK
+Pass: 2017!RutaK
+*/
