7437e63e909331aff596e6c75fd0cf144f562c21
AlbertoMalih <gu.oleg2015@yandex.ru>
2017-05-17 12:35:49 +0300

    optimize code 1

--- src/dispatchers/Dispatcher.java
+++ src/dispatchers/Dispatcher.java
@@ -2,75 +2,28 @@ package dispatchers;
 
 import models.ConsoleHelper;
 import utils.ListUtils;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Administrator1 on 17.05.2017.
  */
 public class Dispatcher {
     public static void main(String[] args) throws Exception {
-        //Timer
-        Date sdate;  //start timer val
-        long edate;  //end timer val
-        long ldate;  //end and start diff
 
         //Выводим список вопросы по очереди
         ArrayList<String> list = ListUtils.genList(16);
-
-        //Статистика
-        int answ = -1, x, y, z, e = 0;
-        char[] vals;
-        char o;
 
         //Are you ready?
         System.out.println("Are you ready? (y/n)");
         String waiter = ConsoleHelper.readWords();
 
         if (waiter.equals("y") || waiter.equals("yes")) {
-
-            System.out.println("Time Start!");
-
-            sdate = new Date();
-            //для каждого значения в списке выполнить метода anw
-            for (int i = 0; i < list.size(); i++) {
-
-                vals = (list.get(i).toCharArray());
-                x = Character.getNumericValue(vals[0]);
-                y = Character.getNumericValue(vals[2]);
-
-                o = vals[1];
-                z = x * y - x - y;
-
-                switch (o){
-                    case '+': z = x + y; break;
-                    case '*': z = x * y; break;
-                    case '/': z = x / y; break;
-                    case '-': z = x - y; break;
-                }
-
-                System.out.println(list.get(i));
-                answ = Integer.parseInt( ConsoleHelper.readWords());
-                if (answ == z) {
-                    System.out.println("ok, answer is: " + z + " next...");
-                }
-                if (answ != z) {
-                    System.out.println("bad... one more time..");
-                    e++;
-                    i = i - 1;
-                }
-            }
-
-            if (answ == -1) {
-                System.out.println("e");
-            }
-            edate = new Date().getTime();
-            ldate = edate - sdate.getTime();
-            ConsoleHelper.printWin(sdate, edate, ldate, e);
-
-        }
-
-        if (waiter.equals("n") || waiter.equals("no")) {
+            runGame(list);
+        } else {
             System.out.println("What you want?");
             waiter = ConsoleHelper.readWords();
             if (waiter.equals("generate")) {
@@ -91,8 +44,63 @@ public class Dispatcher {
                 }
 
             }
-
         }
+    }
+
+    private static void runGame(List<String> list) throws IOException {
+        Date sdate;  //start timer val
+        long edate;  //end timer val
+        long ldate;  //end and start diff
+        //Статистика
+        int answ = -1, x, y, z, e = 0;
+        char[] vals;
+        char o;
+
+        System.out.println("Time Start!");
+
+        sdate = new Date();
+        //для каждого значения в списке выполнить метода anw
+        for (int i = 0; i < list.size(); i++) {
+
+            vals = (list.get(i).toCharArray());
+            x = Character.getNumericValue(vals[0]);
+            y = Character.getNumericValue(vals[2]);
+
+            o = vals[1];
+            z = x * y - x - y;
+
+            switch (o) {
+                case '+':
+                    z = x + y;
+                    break;
+                case '*':
+                    z = x * y;
+                    break;
+                case '/':
+                    z = x / y;
+                    break;
+                case '-':
+                    z = x - y;
+                    break;
+            }
+
+            System.out.println(list.get(i));
+            answ = Integer.parseInt(ConsoleHelper.readWords());
+            if (answ == z) {
+                System.out.println("ok, answer is: " + z + " next...");
+            }
+            if (answ != z) {
+                System.out.println("bad... one more time..");
+                e++;
+                i = i - 1;
+            }
+        }
+        if (answ == -1) {
+            System.out.println("e");
+        }
+        edate = new Date().getTime();
+        ldate = edate - sdate.getTime();
+        ConsoleHelper.printWin(sdate, edate, ldate, e);
     }
 }
 
