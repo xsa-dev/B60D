50fdfd429820529e875bd3260c5ddb8cc0c9aa15
AlbertoMalih <gu.oleg2015@yandex.ru>
2017-05-18 09:06:18 +0300

    пофиксил код подключения к бд и поставил варианты работы с ней

--- src/TestO1.java
+++ src/TestO1.java
@@ -5,9 +5,32 @@ import java.sql.*;
  */
 public class TestO1 {
     public static void main(String[] args) throws Exception {
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-        Connection connection = DriverManager.getConnection("","","");
-    }/
-}/
-/*
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://mysql.alex-savin.myjino.ru:3306/alex-savin_trutak","", ""
+        );
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM users1");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1));
+            System.out.println(resultSet.getInt("id") + "\n");
+        }
+        resultSet.close();
+
+//        statement.executeUpdate(
+//                "INSERT INTO users1(name, date) VALUES ('grgr', CURRENT_DATE);");
+
+        statement.executeUpdate("UPDATE users1 SET date = CURRENT_DATE WHERE id > 3 AND id <= 7");
+        statement.executeUpdate("DELETE FROM users1 WHERE id > 8");
+
+        resultSet = statement.executeQuery("SELECT * FROM users1");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1));
+            System.out.println(resultSet.getInt("id") + "\n");
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
